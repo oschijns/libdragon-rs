fdcff85108907d2d05c75a4d27462b1b99be9e4a
Olivier Schyns <olivier.schyns95@gmail.com>
2024-11-11 17:05:35 +0100

    Fixed name of examples in build.rs files

--- libdragon-examples/vtest/build.rs
+++ libdragon-examples/vtest/build.rs
@@ -4,7 +4,7 @@ fn main() -> Result<()> {
     Build::new()
         .set_env_file(".libdragon-env")
         .set_just_file(".libdragon-just")
-        .set_game_name("JOYPADTEST")
+        .set_game_name("VTEST")
         .enable_rsp_compile()
         .build()
 }
